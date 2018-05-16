0353128b6466a25fb4a2c7b57f5cd2e9d6cbda3a
jgeorge247 <justingeorge247@gmail.com>
2018-05-15 20:35:41 -0500

    first commit

---
+++ Linked_List_Project/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+            list.AddAtStart("Adam");
+            list.AddAtStart("Cat");
+            list.AddAtStart("Mohammed");
+            list.AddAtStart("Shaun");
+            list.AddAtStart("Tom");
+            list.AddAtStart("Mindy");
+            list.AddToEnd("Frenchie");
+            list.AddToEnd("Danny");
+            list.AddToEnd("Jaide");
+
+            list.PrintAllNodes();
+
+            Console.WriteLine("");
+
+            object o = "Frenchie";
+            list.RemoveAll(o);
+
+            list.PrintAllNodes();
+
+        }
+    }
+}
